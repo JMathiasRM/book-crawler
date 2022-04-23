a98f63323d301796456833b1f7e761ec1ac4a50e
João <joao.mathias@driva.com.br>
2022-04-23 00:17:20 -0300

    adding files

---
+++ api.py
@@ -0,0 +1,11 @@
+class BookCrawler:
+    def __init__(self):
+        pass
+
+    # Retorna as informações de N livros de uma categoria
+
+    # Salva as informações de todos os livros de uma categoria
+
+    # Apaga as informações de todos os livros de uma categoria
+
+    # Retorna os livros de uma categoria com estoque abaixo de N
