ea4776e623f94a30e6083be224b9e3ecb14143bc
João <joao.mathias@driva.com.br>
2022-04-25 09:16:09 -0300

    feat: function declarations

--- api.py
+++ api.py
@@ -1,11 +1,25 @@
-class BookCrawler:
+class BookCrawlerAPI:
     def __init__(self):
         pass
 
     # Retorna as informações de N livros de uma categoria
+    ## cat: categoria
+    ## n: número de livros para retornar
+    def livrosCategoria(cat: str, n: int):
+        pass
 
     # Salva as informações de todos os livros de uma categoria
+    ## cat: categoria
+    def salvarCategoria(cat: str):
+        pass
 
     # Apaga as informações de todos os livros de uma categoria
+    ## cat: categoria
+    def apagarCategoria(cat: str):
+        pass
 
     # Retorna os livros de uma categoria com estoque abaixo de N
+    ## cat: categoria
+    ## n: número de livros
+    def estoqueCategoria(cat: str, n: int):
+        pass
