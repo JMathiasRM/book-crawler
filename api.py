0f1ae0148bc05533c35bb0fd3127c0f4ff6de609
João <joao.mathias@driva.com.br>
2022-04-27 20:38:00 -0300

    feat: api finished

--- api.py
+++ api.py
@@ -1,25 +1,94 @@
-class BookCrawlerAPI:
-    def __init__(self):
-        pass
+from typing import Optional
+from unittest.util import strclass
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-    # Retorna as informações de N livros de uma categoria
-    ## cat: categoria
-    ## n: número de livros para retornar
-    def livrosCategoria(cat: str, n: int):
-        pass
+from db import Database
+from crawler import Crawler
 
-    # Salva as informações de todos os livros de uma categoria
-    ## cat: categoria
-    def salvarCategoria(cat: str):
-        pass
+class Livro(BaseModel):
+    titulo: str
+    categoria: str
+    preco: float
+    estoque: int
+    desc: str
+    data: str
 
-    # Apaga as informações de todos os livros de uma categoria
-    ## cat: categoria
-    def apagarCategoria(cat: str):
-        pass
+app = FastAPI()
+crawler = Crawler()
+db = Database()
 
-    # Retorna os livros de uma categoria com estoque abaixo de N
-    ## cat: categoria
-    ## n: número de livros
-    def estoqueCategoria(cat: str, n: int):
-        pass
+@app.get('/')
+async def root():
+    return
+
+class HTTPError(BaseModel):
+    detail: str
+    class Config:
+        schema_extra = {
+            "example": {"detail": "string"},
+        }
+
+@app.get('/search',
+    responses={
+        400: {
+            'model': HTTPError
+        },
+        404: {
+            'model': HTTPError
+        }
+})
+# Retorna as informações de N livros de uma categoria
+## cat: categoria
+## n: número de livros para retornar
+## crawl: forçar crawleamento
+async def livrosCategoria(cat: str, n: int, crawl: bool = False):
+    if crawl:
+        db.deleteCat(cat)
+        crawl_res = crawler.crawl(cat)
+        db.insert(crawl_res)
+    if n<0:
+        raise HTTPException(
+            status_code=400,
+            detail=f"{n} is not a valid amount of books"
+        )
+    livros_categoria = db.fetchN(cat,n)
+    if livros_categoria==[] and n>0:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Books from category '{cat}' not found (did you capitalize the first letters?)"
+        )
+    return livros_categoria
+
+@app.get('/save',
+    responses={
+        400: {
+            'model': HTTPError
+        }
+})
+# Salva as informações de todos os livros de uma categoria
+## cat: categoria
+async def salvarCategoria(cat: str):
+    crawl_res = {}
+    try:
+        crawl_res = crawler.crawl(cat)
+    except:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Unable to crawl books from category '{cat}' (did you capitalize the first letters?)"
+        )
+    else:
+        db.insert(crawl_res)
+
+@app.get('/delete')
+# Apaga as informações de todos os livros de uma categoria
+## cat: categoria
+async def apagarCategoria(cat: str):
+    db.deleteCat(cat)
+
+@app.get('/stock')
+# Retorna os livros de uma categoria com estoque abaixo de N
+## cat: categoria
+## n: número de livros
+async def estoqueCategoria(cat: str, n: int):
+    return db.fetchStockN(cat, n)
